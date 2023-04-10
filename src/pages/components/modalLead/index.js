9322aa1487744ec1f8551ecaa4264fca97afae60
Rafael Oliveira <rafaelmo@pinolab.com.br>
2023-04-10 13:58:42 -0300

    a

--- src/pages/components/modalLead/index.js
+++ src/pages/components/modalLead/index.js
@@ -16,13 +16,12 @@ const ModalLead = () => {
       <div class="full-page-modal">
         {/* <button class="button-close" type="submit" onClick={() => { setModalOpen(false) }}>X</button> */}
         <div class="modal-lead">
-          <img src="/images/favicon.png" width={200}></img>
+          {/* <img src="/images/favicon.png" width={200}></img> */}
           <div class="label-lead">
           <h1>Seja Bem Vindo(a)</h1>
-          <p>Deixe o seu contato para nós.</p>
           </div>
           <input class="input-lead" name="name" type="text" alt="input name" placeholder="Digite seu Nome..." onChange={onChangeInput} value={dataForm.name}></input>
-          <input class="input-lead" name="email" type="name" alt="input email" placeholder="Digite seu E-mail..." onChange={onChangeInput} value={dataForm.email}></input>
+          {/* <input class="input-lead" name="email" type="name" alt="input email" placeholder="Digite seu E-mail..." onChange={onChangeInput} value={dataForm.email}></input> */}
           <input class="input-lead" name="phone" type="number" alt="input phone" placeholder="Digite seu Telefone..." onChange={onChangeInput} value={dataForm.phone}></input>
           <button class="button-submit" type="submit" onClick={() => { setModalOpen(false) }}>VER PLANOS</button>
         </div>
--- src/pages/start/packages/index.js
+++ src/pages/start/packages/index.js
@@ -9,21 +9,23 @@ const packageTwo = { title: "Standart 12 meses", products: ["Arte Logo", "Site M
 const packageTree = { title: "Professional 12 meses", products: ["Arte Logo", "Branding", "Site Multipage", "Social Media 3x Semanal", "Blog Semanal", "Arte Logo e Site Multipage e texto longo", "Arte Logo", "Site Multipage"], price: "R$ 399,00" };
 
 const Packages = () => {
-        return (
+    return (
+        <>
+            <ModalLead></ModalLead>
             <div class="full-page-start">
-                <ModalLead></ModalLead>
-                <div class="full-content-slider">
-                <h1>Pacotes sugeridos para você:</h1>
-                    <PackItens title={packageOne.title} products={packageOne.products} price={packageOne.price}></PackItens>
-                    <PackItens title={packageTwo.title} products={packageTwo.products} price={packageTwo.price}></PackItens>
-                    <PackItens title={packageTree.title} products={packageTree.products} price={packageTree.price}></PackItens>
-                    <div class="card-box"><button class="custom-button">Pacote Personalizado</button></div>
-    
-                </div>
+                {/* <div class="full-content-slider" > */}
+                <h1 style={{fontSize: "22px", color:"#FFFFFF"}}>Pacotes sugeridos para você:</h1>
+                <PackItens title={packageOne.title} products={packageOne.products} price={packageOne.price}></PackItens>
+                <PackItens title={packageTwo.title} products={packageTwo.products} price={packageTwo.price}></PackItens>
+                <PackItens title={packageTree.title} products={packageTree.products} price={packageTree.price}></PackItens>
+                <div class="card-box"><button class="custom-button">Pacote Personalizado</button></div>
+
+                {/* </div> */}
                 <StepsShow step={2}></StepsShow>
             </div>
-        )
-    
+        </>
+    )
+
 
 
 }
@@ -33,20 +35,20 @@ export default Packages
 const PackItens = (props) => {
 
     return (
-        <>
-            <div class="card-box">
-                <div class="package-card">
-                    <div class="package-title">{"Pacote " + props.title}</div>
-                    <div class="package-midle">
-                        <div class="package-icon"></div>
-                        <div class="package-price">{props.price}<span class="text-small"> /mês</span></div>
-                    </div>
-                    <div class="package-products">{props.products.sort().map((e) => (
-                        <div class="tag-product">{e}</div>
-                    ))}</div>
+
+        <div class="card-box">
+            <div class="package-card">
+                <div class="package-title">{"Pacote " + props.title}</div>
+                <div class="package-midle">
+                    <div class="package-icon"></div>
+                    <div class="package-price">{props.price}<span class="text-small"> /mês</span></div>
                 </div>
+                <div class="package-products">{props.products.sort().map((e) => (
+                    <div class="tag-product">{e}</div>
+                ))}</div>
             </div>
-        </>
+        </div>
+
     )
 
 }
